50122e1735bb34c9880d904d45ad0f855dd11c61
abi1402 <119950900+abi1402@users.noreply.github.com>
2023-07-20 08:59:27 +0530

    Update Face Detection.py

--- Face Detection.py
+++ Face Detection.py
@@ -1,33 +1,37 @@
 import cv2
-import os
-dataset = "dataset"
-name = "champ"
 
-path = os.path.join(dataset,name)
-if not os.path.isdir(path):
-    os.mkdir(path)
-
-(width,height) = (130,100)
 alg = "haarcascade_frontalface_default.xml"
-haar_cascade = cv2.CascadeClassifier(alg)
-cam = cv2.VideoCapture(2)
 
-count = 1
-while count < 31:
-    print(count)
-    _,img = cam.read()
-    grayImg = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    face = haar_cascade.detectMultiScale(grayImg,1.3,4)
-    for (x,y,w,h) in face:
-        cv2.rectangle(img,(x,y),(x+w,y+h), (0,255,0),2)
-        faceOnly = grayImg[y:y+h,x:x+w]
-        resizeImg = cv2.resize(faceOnly,(width,height))
-        cv2.imwrite("%s/%s.jpg" %(path,count),resizeImg)
-        count+=1
-    cv2.imshow("FaceDetection",img)
+haar_cascade = cv2.CascadeClassifier(alg)
+
+cam = cv2.VideoCapture(0)
+
+total_faces = 0
+correct_faces = 0
+
+while True:
+    _, img = cam.read()
+    grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    face = haar_cascade.detectMultiScale(grayImg, 1.3, 4)
+    
+    for (x, y, w, h) in face:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        total_faces += 1
+
+    # Increment correct_faces if the face is detected correctly
+    if len(face) > 0:
+        correct_faces += 1
+    
+    cv2.imshow("FaceDetection", img)
     key = cv2.waitKey(10)
     if key == 27:
         break
-print("Image Captured succssfully")
+
+# Calculate the accuracy after the face detection loop completes
+if total_faces > 0:
+    accuracy = (correct_faces / total_faces) * 100
+    print(f"Total Accuracy: {accuracy:.2f}%")
+
 cam.release()
 cv2.destroyAllWindows()
+
